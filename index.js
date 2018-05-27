f516e1178465f6954bd99050c4d7f96880d00ef2
Jose Vicente Bañuls García <banulsgarcia.josevicente@gmail.com>
2018-05-27 13:38:43 +0200

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var MongoClient = require('mongodb').MongoClient;
 // Connect to the db
 MongoClient.connect("mongodb://localhost:27017/AnimaliaPro", function(err, db) {
   if(!err) {
-    console.log("We are connected");
+    console.log("We are connected"+db);
     var collection = db.db.collection('users');
     var doc1 = { 'name': 'test1', 'password':'1234' };
     var lotsOfDocs = [{ 'name': 'test1', 'password':'1234','role':1 }, { 'name': 'test2', 'password':'1234', 'role':2 }];
