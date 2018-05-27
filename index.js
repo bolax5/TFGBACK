813a89051ad3bdf4792da4ce7697056575d75ef2
Jose Vicente Bañuls García <banulsgarcia.josevicente@gmail.com>
2018-05-27 20:34:42 +0200

    try again

--- index.js
+++ index.js
@@ -30,27 +30,4 @@ app.get('/login', async (req,res)=>{
     res.send("Correct")
 });
 
-app.listen(4444,'0.0.0.0',()=>console.log("server listening on 4444"));
-
-// Connect to the db
-// MongoClient.connect("mongodb://localhost:27017/AnimaliaPro", function(err, client) {
-//   if(!err) {
-//     console.log("We are connected");
-//     var db = client.db('AnimaliaPro');
-//     var collection = db.collection('user');
-//     var lotsOfDocs = [{ 'name': 'test1', 'password':'1234','role':1 }, { 'name': 'test2', 'password':'1234', 'role':2 }];
-
-//     collection.insert(lotsOfDocs, { w: 1 }, function (err, result) {
-//         if(err){
-//             console.log("error:  "+ err.message);
-//         }
-//      });
-
-//     client.close();
-
-
-//   }
-//   else {
-//       console.log(err);
-//   }
-// });
+app.listen(8081,'0.0.0.0',()=>console.log("server listening on 8081"));
